711c04a6a771cef540ce29a19750108dd2b5b077
andy <huangliling@hrgame.com.cn>
2016-01-27 10:50:59 +0800

    LockfreeQueue implementation

--- container/queue/lockfree_queue.go
+++ container/queue/lockfree_queue.go
@@ -1,5 +1,55 @@
 package queue
 
 import (
-
+	"sync/atomic"
+	"unsafe"
 )
+
+func NewLockfreeQueue() *LockfreeQueue {
+	var lfq LockfreeQueue
+	lfq.head = unsafe.Pointer(&lfq.dummy)
+	lfq.tail = unsafe.Pointer(&lfq.dummy)
+	return &lfq
+}
+
+type LockfreeQueue struct {
+	head  unsafe.Pointer
+	tail  unsafe.Pointer
+	dummy lfqNode
+}
+
+func (lfq *LockfreeQueue) Pop() interface{} {
+	for {
+		h := atomic.LoadPointer(&lfq.head)
+		rh := (*lfqNode)(h)
+		n := (*lfqNode)(atomic.LoadPointer(&rh.next))
+		if n != nil {
+			if atomic.CompareAndSwapPointer(&lfq.head, h, rh.next) {
+				return n.val
+			} else {
+				continue
+			}
+		} else {
+			return nil
+		}
+	}
+}
+
+func (lfq *LockfreeQueue) Push(val interface{}) {
+	node := unsafe.Pointer(&lfqNode{val: val})
+	for {
+		t := atomic.LoadPointer(&lfq.tail)
+		rt := (*lfqNode)(t)
+		if atomic.CompareAndSwapPointer(&rt.next, nil, node) {
+			atomic.StorePointer(&lfq.tail, node)
+			break
+		} else {
+			continue
+		}
+	}
+}
+
+type lfqNode struct {
+	val  interface{}
+	next unsafe.Pointer
+}
