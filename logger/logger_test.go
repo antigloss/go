23dcf4ff2293aed670699459ed97697a83cca053
huangliling <huangliling@xd.com>
2020-09-30 18:11:28 +0800

    update logger_test.go

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -1,37 +1,30 @@
 package logger
 
 import (
-	"fmt"
-	"runtime"
-	"sync"
 	"testing"
 )
 
-func BenchmarkLogger(b *testing.B) {
-	//Init("./log", 10, 2, 2, false)
-
-	benchmark(b, 1, 1, 100000)
-	benchmark(b, 1, 100000, 1)
-	benchmark(b, 2, 100000, 1)
-	benchmark(b, 4, 100000, 1)
-	benchmark(b, 8, 100000, 1)
+func init() {
+	Init(&Config{
+		LogDir:          "./logs",
+		LogFileMaxSize:  200,
+		LogFileMaxNum:   500,
+		LogFileNumToDel: 50,
+		LogLevel:        LogLevelInfo,
+		LogDest:         LogDestFile,
+		Flag:            ControlFlagLogLineNum,
+	})
 }
 
-func benchmark(b *testing.B, nProcs, nGoroutines, nWrites int) {
-	runtime.GOMAXPROCS(nProcs)
-	bn := fmt.Sprintf("%d Procs %d Goroutines each makes %d writes", nProcs, nGoroutines, nWrites)
-
-	b.Run(bn, func(b *testing.B) {
-		var wg sync.WaitGroup
-		for i := 0; i < nGoroutines; i++ {
-			wg.Add(1)
-			go func() {
-				for j := 0; j < nWrites; j++ {
-					Info("Failed to find player! uid=%d plid=%d cmd=%s xxx=%d", 1234, 678942, "getplayer", 102020101)
-				}
-				wg.Add(-1)
-			}()
+func BenchmarkLogger(b *testing.B) {
+	b.Run("benchmarkInfo", func(b *testing.B) {
+		for i := 0; i != b.N; i++ {
+			Info("Failed to find player! uid", 1234, "plid", 678942, "cmd=getplayer xxx", 102020101)
 		}
-		wg.Wait()
+	})
+	b.Run("benchmarkInfof", func(b *testing.B) {
+		for i := 0; i != b.N; i++ {
+			Infof("Failed to find player! uid=%d plid=%d cmd=%s xxx=%d", 1234, 678942, "getplayer", 102020101)
+		}
 	})
 }
