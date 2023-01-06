af19fe73606b30423a35fc1fda5212d2ea19990f
huangliling <huangliling@xd.com>
2023-01-06 17:15:32 +0800

    add copyright

--- conf/examples/example_test.go
+++ conf/examples/example_test.go
@@ -1,3 +1,22 @@
+/*
+ *
+ * Copyright (C) 2023 Antigloss Huang (https://github.com/antigloss) All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package examples_test
 
 import (
@@ -44,7 +63,7 @@ func ExampleConfFromEnv() {
 // 从文件中读取配置的示例
 func ExampleConfFromFile() {
 	c := conf.New[ExampleConfig](
-		conf.WithTagName("json"),                                                              // 设置反序列化时使用的 TagName ，默认是 mapstructure
+		conf.WithTagName("json"), // 设置反序列化时使用的 TagName ，默认是 mapstructure
 		conf.WithStores(file.New(file.WithConfigPaths(file.ConfigPath{Path: "./conf.yaml"}))), // 创建从文件中读取配置的 Store 对象，并传入 ConfigParser
 	)
 
